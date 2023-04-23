6e0f7881a41e253cb3499713d46e11d4fe79530e
ANDERSON <i202020454@cibertec.edu.pe>
2023-04-22 20:21:58 -0500

    Actividad 1

---
+++ src/main/java/org/example/Empleado.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class calcularSalarioBruto {
+    class Empleado {
+        public enum TipoEmpleado {
+            VENDEDOR, ENCARGADO
+        }
+
+        public static class CAException extends Exception {
+            public CAException(String errorMessage) {
+                super(errorMessage);
+            }
+        }
+
+        public float calculaSalarioBruto(TipoEmpleado tipo, float ventasMes, float horasExtra) throws CAException {
+            if (tipo == null || ventasMes < 0f || horasExtra < 0f) {
+                throw new CAException("Los parámetros no pueden ser nulos o negativos");
+            }
+
+            float salarioBase = tipo == TipoEmpleado.VENDEDOR ? 1000f : 1500f;
+            float prima = 0f;
+            if (ventasMes >= 1000f) {
+                prima = ventasMes >= 1500f ? 200f : 100f;
+            }
+            float salarioHorasExtra = horasExtra * 20f;
+            return salarioBase + prima + salarioHorasExtra;
+        }
+
+        public float calculaSalarioNeto(float salarioBruto) throws CAException {
+            if (salarioBruto < 0f) {
+                throw new CAException("El salario bruto no puede ser negativo");
+            }
+            float retencion;
+            if (salarioBruto < 1000f) {
+                retencion = 0f;
+            } else if (salarioBruto < 1500f) {
+                retencion = 0.16f;
+            } else {
+                retencion = 0.18f;
+            }
+            return salarioBruto * (1 - retencion);
+        }
+    }
+
+}
